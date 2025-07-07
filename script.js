de1ad48932f0fbacd3601b9b32f59b16cf511a55
Devansh Chandra <155964011+Devansh149@users.noreply.github.com>
2025-07-07 21:42:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-let input=document.querySelector(".num");
-let count=parseInt(input.value);
-for (var i = 0; i < document.querySelectorAll("button").length; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", function () {
-    switch(this.textContent){
-      case "+":
-        count++;
-        input.value=count;
-        break;
-      case "-":
-        count--;
-        input.value=count;
-        break;
-      case "Reset":
-        count=0;
-        input.value=count;
-        break;
-      default  : console.log("Unkown");
+var input = document.getElementById("count");
+var value = 0;
 
-    }
-  })
-}
+document.getElementsByClassName("click")[0].addEventListener("click", function () {
+    value++;
+    input.value = value;
+});
+document.getElementsByClassName("reset")[0].addEventListener("click", function () {
+    value=0;
+    input.value = value;
+});
